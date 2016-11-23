0c2a2e03a36a0756924ed0e9fc11291dd93d4d9b
ayoub92 <ayoubovsky@gmail.com>
2016-11-23 01:51:39 +0000

    ajouter code envoyé

---
+++ insert_contact.php
@@ -0,0 +1,34 @@
+<?php
+
+if(empty($_POST['nom']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['sujet']) || empty($_POST['message'])) {
+echo "errouuuur";
+}
+
+$nom=$_POST['nom'];
+$email=$_POST['email'];
+$tel=$_POST['phone'];
+$sujet=$_POST['sujet'];
+$message=$_POST['message'];
+
+$to='ayoubovsky@gmail.com';
+
+$headers = 'From: '.$email."\r\n" .
+	'Reply-To: '.$email."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+$sujet = $sujet;
+$body='Vous avez reçu un nouveau message à partir du formulaire de notification sur votre site web.'."\n\n";
+$body.='Nom: '.$nom."\n";
+$body.='Email: '.$email."\n";
+$body.='Telephone: '.$tel."\n";
+$body.='Sujet: '.$sujet."\n";
+$body.='Message: '."\n".$message."\n";
+$url='message envoye.php';
+$url1='erreur.php';
+if(mail($to, $subject, $body, $headers)) {
+	 echo "mchaaaaaaaaaa";
+
+} else {
+	 echo "ch blan";
+}
+
+?>
